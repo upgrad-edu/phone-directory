53537ee9070eda636a462687ac58d5538c783bd0
Srishti Gupta <mail@srishtigupta.in>
2018-09-09 10:58:53 +0530

    Dynamic Content: Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import Header from './Header.js';
 import './App.css';
 
 class App extends Component {
+
   render() {
+
+    let subscribers = [
+      {
+        id: 1,
+        name: "Shilpa Bhat",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Srishti Gupta",
+        phone: "9999999999"
+      }
+    ];
+
     return (
       <div>
         <Header />
@@ -14,6 +29,15 @@ class App extends Component {
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            })
+          }
         </div>
       </div>
     );
