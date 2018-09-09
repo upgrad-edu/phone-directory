f39c3df2c227957bdc3294795efae0e66eae429b
Srishti Gupta <mail@srishtigupta.in>
2018-09-09 13:57:04 +0530

    Props: Passed 'heading' to Header component as props

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
 
     return (
       <div>
-        <Header />
+        <Header heading="Phone Directory" />
         <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
 
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Header.css';
 
-const Header = function () {
+const Header = function (props) {
     return (
         <div className="header">
-            Phone Directory
+            {props.heading}
         </div>
     )
 }
